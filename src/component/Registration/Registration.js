237c2079be5e37616f811f130029df12b4639511
Mim <fatemamim28@gmail.com>
2021-10-20 14:45:41 +0600

    ok

--- src/component/Registration/Registration.js
+++ src/component/Registration/Registration.js
@@ -6,16 +6,17 @@ import { useHistory } from 'react-router';
 
 
 const Registration = () => {
-    const[name , setName] = useState('');
     const history = useHistory();
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error ,setError] = useState('');
-    const { signInWithEmailPassword } = useAuth();
+    const {registration } = useAuth();
 
     const redirect_uri =  '/home';
 
 
+    
     const handleName = e =>{
         setName(e?.target?.value);
     }
@@ -33,10 +34,7 @@ const Registration = () => {
             setError('Password must be in 6 character or more then ')
             return;
         }
-        if (!/(?=.*[A-Z].*[A-Z])/.test(password)){
-            setError("Password Must be contain 2 upper case")
-        }
-        signInWithEmailPassword(email ,password);
+        registration(email ,password , name);
         history.push(redirect_uri);
 
 
@@ -47,16 +45,16 @@ const Registration = () => {
                 <h2 className="text-center">Please <span className="supports-Name">Register</span></h2>
                 <Form onSubmit={handleRegistration}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control onBlur={handleName} type="text" placeholder="Enter your name" required />
+                        <Form.Label>You Name :</Form.Label>
+                        <Form.Control onBlur={handleName} type="text" placeholder="Enter name" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
+                        <Form.Label>Email address :</Form.Label>
                         <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" required />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label>Password :</Form.Label>
                         <Form.Control onBlur={handlePassword} type="password" placeholder="Password" required />
                     </Form.Group>
                     <p className="text-danger">{error}</p>
@@ -64,7 +62,7 @@ const Registration = () => {
                         Registration
                     </Button>
                 </Form>
-                <p className="mt-2">Already have an account ? Or Gimail Login <Link to="/login">Login</Link></p>
+                <p className="mt-2">Already have an account ? Or Gmail Login <Link to="/login">Login</Link></p>
            </Container>
             
         </div>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,28 +1,46 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged , createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword,  updateProfile} from "firebase/auth";
 import { useEffect, useState } from "react";
 import initializeAuthentication from '../Firebase/firebase.inti';
 
 
+
 initializeAuthentication();
 const useFirebase = () =>{
-    const {error ,setError} = useState('')
+    
+    const [error ,setError] = useState('');
     const [isLoading, setIsLoading] = useState(true)
     const [user ,setUser] = useState({});
     const auth = getAuth();
 
+    // google sign in 
     const signInWithGoogle = () =>{
         setIsLoading(true)
         const googleProvider = new GoogleAuthProvider();
         return signInWithPopup(auth, googleProvider)
         
     }
-    const signInWithEmailPassword = (email , password) =>{
+    
+    // create new user
+    const registration = (email , password ,name) =>{
         createUserWithEmailAndPassword(auth ,email,password)
         .then(result =>{
+            setUserName(name);
+            
         })
         
     }
+
+    // set name
+    const setUserName = (name) =>{
+        updateProfile(auth.currentUser , {displayName : name})
+        .then(res => {
+            window.location.reload()
+        })
+    }
+
     
+    
+    // login
     const logIn = (email , password) =>{
         signInWithEmailAndPassword(auth , email, password)
         .then(result => {
@@ -44,7 +62,8 @@ const useFirebase = () =>{
             }
             setIsLoading(false)
         });
-    },[])
+    },[]);
+    // logout
     const logOut =() =>{
         setIsLoading(true)
         signOut(auth)
@@ -58,7 +77,7 @@ const useFirebase = () =>{
         isLoading,
         signInWithGoogle,
         logOut,
-        signInWithEmailPassword,
+        registration,
         logIn,
         
 
